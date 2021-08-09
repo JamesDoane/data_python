857df1b86245cdeb3060869847bc234a54be4bb1
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-09 13:56:59 -0600

    Initial commit up to part 2 completed

---
+++ functions.py
@@ -0,0 +1,13 @@
+def hello_world() :
+    print("Hello World")
+
+def print_name() :
+    name = input("What's your name? ")
+    print("Your name is " + str(name))
+
+
+def add(a, b) :
+    return a+b
+
+sum = add(3,3)
+print(sum)
